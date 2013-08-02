0fa614a58a46163bbe1dcab4690d47ec544c8665
mollerhoj <mollerhoj3@gmail.com>
2013-08-02 19:38:42 +0200

    CommandT added

---
+++ setuplinks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+ln -s vimrc ../.vimrc
+ln -s zshrc ../.zshrc
+ln -s vim ../.vim
+ln -s vim-tmp ../.vim-tmp
+ln -s vim-tmp-to-die ../.vim-tmp-to-die
+ln -s gitignore ../.gitignore
+ln -s gitconfig ../.gitconfig
+ln -s tmux.conf ../.tmux.conf
