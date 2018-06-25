2d20a153688e6e63ea7a40720ad1830c4a3a09a9
mollerhoj <mollerhoj3@gmail.com>
2018-06-25 14:39:08 +0200

    update script

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
-cd ..
-ln -s -f dotfiles/vimrc .vimrc
-ln -s -f dotfiles/zshrc .zshrc
-ln -s -f dotfiles/vim .vim
-ln -s -f dotfiles/vim-tmp .vim-tmp
-ln -s -f dotfiles/vim-tmp-to-die .vim-tmp-to-die
-ln -s -f dotfiles/gitignore .gitignore
-ln -s -f dotfiles/gitconfig .gitconfig
-ln -s -f dotfiles/tmux.conf .tmux.conf
+ln -s -f ~dotfiles/vimrc ~/.vimrc
+ln -s -f ~dotfiles/zshrc ~/.zshrc
+ln -s -f ~dotfiles/vim ~/.vim
+ln -s -f ~dotfiles/vim-tmp ~/.vim-tmp
+ln -s -f ~dotfiles/vim-tmp-to-die ~/.vim-tmp-to-die
+ln -s -f ~dotfiles/gitignore ~/.gitignore
+ln -s -f ~dotfiles/gitconfig ~/.gitconfig
+ln -s -f ~dotfiles/tmux.conf ~/.tmux.conf
+ln -s -f ~dotfiles/nvimrc ~/.nvimrc
